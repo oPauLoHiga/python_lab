deaf35fd3ca9e8817c8c380caea31c43df4dd06d
Paulo Higa <paulojhiga@gmail.com>
2025-11-28 17:10:23 -0300

    Update rpg_simple.py
    Tentativas falhas

--- Zer0_RPG/rpg_simple.py
+++ Zer0_RPG/rpg_simple.py
@@ -1,51 +1,34 @@
-from random import randint
-
-lista_npc_randon = []
-
-def Criar_monstro():
-    level = randint(1, 10)
-
-    novo_npc = {
-        "nome" : f"Monstro #{level}",
-        "level" : level,
-        "hp" : 100 * level,
-        "dano" : 20 * level
-        }
-    lista_npc_randon.append(novo_npc)
-
-def gerar_monstro(n_monstro):
-
-    for x in range(n_monstro):
-        Criar_monstro()
+dados_personagens = {
+    "jogadores": [
+            {"id":1 ,"nome": "guerreiro", "level": 1, "hp": 200, "hp_max": 200, "dano": 15},
+            {"id":2, "nome": "mago", "level": 1, "hp": 100, "hp_max": 100, "dano": 30}
+        ],
+        "inimigos": [
+            {"id":1 ,"nome": "goblin", "level": 1, "hp": 100, "hp_max": 100, "dano": 15},
+            {"id":2 ,"nome": "bruxa", "level": 1, "hp": 90, "hp_max": 90, "dano": 20}
+        ]
+    }
+jogador = dados_personagens["jogadores"]
+inimigo  = dados_personagens["inimigos"]
 
 
-jogador = [
-    {"nome": "guerreiro", "level": 1, "hp": 200, "hp_max": 200, "dano": 15},
-    {"nome": "mago", "level": 1, "hp": 100, "hp_max": 100, "dano": 30}
-]
-npcs = [
-    {"nome" : "goblin", "level" : 1, "hp": 100, "hp_max": 100, "dano": 15},
-    {"nome" : "bruxa", "level" : 1, "hp": 90, "hp_max": 90, "dano": 20}
-]
+def exibir_personagens_id(dados_personagens, id_busca):
+    for personagem in dados_personagens:
+        if personagem["id"] == id_busca :
+            exibir_informacoes_personagens(jogador)
+        elif personagem["id"] == id_busca:
+            exibir_informacoes_personagens(inimigo)
+        return
+    print("Personagem não encontrado!")
 
-def Exibir_npcs(npcs):
-    for npc in npcs:
-        print(f"Nome: {npc['nome']} // Level: {npc['level']} // hp: {npc['hp']} // dano: {npc['dano']}")
+def exibir_informacoes_personagens(dados_personagens):
+    for personagem in dados_personagens:
+        print(f"Nome: {personagem['nome']} // Level: {personagem['level']} // hp: {personagem['hp']} // dano: {personagem['dano']}")
 
-def Exibir_player(jogador):
-    for jog in jogador:
-        # print(jog["nome"], jog["level"], jog["hp"], jog["dano"]) # posição
-        print(f"Nome: {jog['nome']} // Level: {jog['level']} // hp: {jog['hp']} // dano: {jog['dano']}")
-
-
-gerar_monstro(2)
-def Mostrar_monstro():
-    for npc in lista_npc_randon:
-        print(f"Nome: {npc['nome']} // Level: {npc['level']} // hp: {npc['hp']} // dano: {npc['dano']}")
-
-Mostrar_monstro()
-# Exibir_npcs(npcs)
-# Exibir_player(jogador)
+# exibir_informacoes_personagens(inimigo)
+# exibir_informacoes_personagens(jogador)
+exibir_personagens_id(jogador, 2)
+# exibir_personagens_id(inimigo, 2)
 
 
 
